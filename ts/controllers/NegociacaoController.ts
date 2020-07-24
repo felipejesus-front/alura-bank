c4a10043c4eced3390b997816f89c2e5d8ec7dc1
Felipe <felipe.campos@al.resilia.com.br>
2020-07-24 09:38:02 -0300

    Guardando as negociações

--- ts/controllers/NegociacaoController.ts
+++ ts/controllers/NegociacaoController.ts
@@ -3,6 +3,7 @@ class NegociacaoController{
     private _inputData: HTMLInputElement;
     private _inputQuantidade: HTMLInputElement;
     private _inputValor: HTMLInputElement;
+    private _negociacoes = new Negociacoes();
 
     constructor(){
 
@@ -21,7 +22,9 @@ class NegociacaoController{
             parseInt(this._inputQuantidade.value),
             parseFloat(this._inputValor.value)
         )
+        
+        this._negociacoes.adiciona(negociacao);
 
-        console.log(negociacao);
+        
     }
 }
