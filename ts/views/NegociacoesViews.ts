8ea852c38815d26c4a8165b2f681dae4c24fc321
Felipe <felipe.campos@al.resilia.com.br>
2020-07-27 00:24:39 -0300

    Herança, reaproveitamento de código

--- ts/views/NegociacoesViews.ts
+++ ts/views/NegociacoesViews.ts
@@ -1,18 +1,7 @@
-class NegociacoesView{
+class NegociacoesView extends View<Negociacoes> {
 
-    private _elemento: Element;
 
-    constructor(seletor:string){
-        this._elemento = document.querySelector(seletor)
-
-    }
-
-    update(model:Negociacoes): void{
-
-        this._elemento.innerHTML = this.template(model);
-    }
-
-    template(model:Negociacoes): string {
+    template(model: Negociacoes): string {
 
         return `
         <table class="table table-hover table-bordered">
