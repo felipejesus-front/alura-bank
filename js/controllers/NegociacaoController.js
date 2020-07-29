0f0ce5464bbc160e1c40f0f6ad4774b986eb07e3
Felipe <felipe.campos@al.resilia.com.br>
2020-07-28 21:35:45 -0300

    organizando modulos em barris (barrels)

--- js/controllers/NegociacaoController.js
+++ js/controllers/NegociacaoController.js
@@ -1,28 +1,22 @@
-System.register(["../views/NegociacoesViews", "../views/MensagemView", "../models/Negociacoes", "../models/negociacao"], function (exports_1, context_1) {
+System.register(["../views/index", "../models/index"], function (exports_1, context_1) {
     "use strict";
     var __moduleName = context_1 && context_1.id;
-    var NegociacoesViews_1, MensagemView_1, Negociacoes_1, negociacao_1, NegociacaoController;
+    var index_1, index_2, NegociacaoController;
     return {
         setters: [
-            function (NegociacoesViews_1_1) {
-                NegociacoesViews_1 = NegociacoesViews_1_1;
+            function (index_1_1) {
+                index_1 = index_1_1;
             },
-            function (MensagemView_1_1) {
-                MensagemView_1 = MensagemView_1_1;
-            },
-            function (Negociacoes_1_1) {
-                Negociacoes_1 = Negociacoes_1_1;
-            },
-            function (negociacao_1_1) {
-                negociacao_1 = negociacao_1_1;
+            function (index_2_1) {
+                index_2 = index_2_1;
             }
         ],
         execute: function () {
             NegociacaoController = class NegociacaoController {
                 constructor() {
-                    this._negociacoes = new Negociacoes_1.Negociacoes();
-                    this._negociacoesView = new NegociacoesViews_1.NegociacoesView('#negociciacoesView');
-                    this._mensagemView = new MensagemView_1.MensagemView('#mensagemView');
+                    this._negociacoes = new index_2.Negociacoes();
+                    this._negociacoesView = new index_1.NegociacoesView('#negociciacoesView');
+                    this._mensagemView = new index_1.MensagemView('#mensagemView');
                     this._inputData = $('#data');
                     this._inputQuantidade = $('#quantidade');
                     this._inputValor = $('#valor');
@@ -30,7 +24,7 @@ System.register(["../views/NegociacoesViews", "../views/MensagemView", "../model
                 }
                 adiciona(event) {
                     event.preventDefault();
-                    const negociacao = new negociacao_1.Negociacao(new Date(this._inputData.val().replace(/-/g, ',')), parseInt(this._inputQuantidade.val()), parseFloat(this._inputValor.val()));
+                    const negociacao = new index_2.Negociacao(new Date(this._inputData.val().replace(/-/g, ',')), parseInt(this._inputQuantidade.val()), parseFloat(this._inputValor.val()));
                     this._negociacoes.adiciona(negociacao);
                     this._negociacoesView.update(this._negociacoes);
                     this._mensagemView.update('Negociação Adicionada com Sucesso!');
---
+++ js/models/index.js
@@ -0,0 +1,23 @@
+System.register(["./negociacao", "./Negociacoes"], function (exports_1, context_1) {
+    "use strict";
+    var __moduleName = context_1 && context_1.id;
+    function exportStar_1(m) {
+        var exports = {};
+        for (var n in m) {
+            if (n !== "default") exports[n] = m[n];
+        }
+        exports_1(exports);
+    }
+    return {
+        setters: [
+            function (negociacao_1_1) {
+                exportStar_1(negociacao_1_1);
+            },
+            function (Negociacoes_1_1) {
+                exportStar_1(Negociacoes_1_1);
+            }
+        ],
+        execute: function () {
+        }
+    };
+});
